17316745779c7849169c6e31193131f662101fec
therootishere00-ux <theroot.ishere00@gmail.com>
2026-04-26 11:20:04 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SWGOH AI",
+  description: "AI Assistant for SWGOH",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
