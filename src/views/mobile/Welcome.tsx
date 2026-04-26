b46d1c8b1fb224fb2690a2f54b95a9ab27dcf315
therootishere00-ux <theroot.ishere00@gmail.com>
2026-04-26 11:21:12 +0300

    Create Welcome.tsx

---
+++ src/views/mobile/Welcome.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-white text-black">
+      <p>Welcome Mobile View</p>
+    </div>
+  );
+}
